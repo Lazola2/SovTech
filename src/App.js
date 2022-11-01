23ea42792c57a2ade80dba5be67106b0d603b00b
Lazola2 <lazolamakubalo271@gmail.com>
2022-11-01 23:28:54 +0200

    added social icons with style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/Navbar/NavBar';
+import SocialIcons from './components/SocialIcons/SocialIcons';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <SocialIcons/>
     </div>
   );
 }
